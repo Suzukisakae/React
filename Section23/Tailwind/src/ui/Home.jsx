07faeaf9613d83a8acafd1223fc32c8b4829cf42
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-04-08 15:14:15 +0700

    learn tailwind

--- Section23/Tailwind/src/ui/Home.jsx
+++ Section23/Tailwind/src/ui/Home.jsx
@@ -2,7 +2,7 @@ import CreateUser from '../features/user/CreateUser';
 
 function Home() {
   return (
-    <div className="my-10 px-4 text-center sm:my-16">
+    <div className="my-10 px-4 text-center sm:my-16 ">
       <h1 className="mb-8  text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
@@ -10,7 +10,6 @@ function Home() {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-
       <CreateUser />
     </div>
   );
