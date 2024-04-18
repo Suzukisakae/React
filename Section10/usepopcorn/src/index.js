be57aac13cf5588bcd39bee71cbdf629d8db1479
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-04-18 22:05:32 +0700

    Refactor StarRating component and add PropTypes validation

--- Section10/usepopcorn/src/index.js
+++ Section10/usepopcorn/src/index.js
@@ -2,12 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { useState } from "react";
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+function Test() {
+  const [rating, setRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        maxRating={5}
+        color="#f78"
+        size={64}
+        className="test"
+        defaultRating={3}
+        onSetRating={setRating}
+      />
+      <p>This movie was rated {rating} stars</p>
+    </div>
+  );
+}
+
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={10}
+      color="#f49"
+      size={64}
+      className="test"
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
