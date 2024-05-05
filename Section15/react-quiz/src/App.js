3583d261007a734f745c74eb35cd0444927f19b4
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-05-05 13:04:29 +0700

    handling a new answers

--- Section15/react-quiz/src/App.js
+++ Section15/react-quiz/src/App.js
@@ -12,6 +12,8 @@ const initialState = {
   // 'loading', 'ready', 'error', 'active', 'inactive',...
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -31,13 +33,23 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error(`Unrecognized action: ${action.type}`);
   }
 }
 
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -60,7 +72,13 @@ function App() {
             dispatch={dispatch}
           />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            answer={answer}
+            dispatch={dispatch}
+          />
+        )}
       </Main>
     </div>
   );
--- Section15/react-quiz/src/components/Options.js
+++ Section15/react-quiz/src/components/Options.js
@@ -1,8 +1,20 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
-      {question.options.map((options) => (
-        <button className="btn btn-option" key={options}>
+      {question.options.map((options, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          disabled={hasAnswered}
+          key={options}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {options}
         </button>
       ))}
