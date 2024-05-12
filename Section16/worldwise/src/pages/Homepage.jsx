a0a5cfae1495dae4440bdcd8491561be06a5a7e2
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-05-12 23:24:29 +0700

    link, navlink

--- Section16/worldwise/src/pages/Homepage.jsx
+++ Section16/worldwise/src/pages/Homepage.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-  return <div>Home pages</div>;
+  return (
+    <div>
+    <PageNav />
+      <h1>Home page</h1>
+      <Link to="/product">Product</Link>
+    </div>
+  );
 }
 
 export default Homepage;
