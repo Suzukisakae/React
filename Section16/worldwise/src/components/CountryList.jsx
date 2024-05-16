e83d4acd1cfc98d16df6ea7f7b591dd2ae35d185
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-05-17 03:54:48 +0700

    Implement country list

---
+++ Section16/worldwise/src/components/CountryList.jsx
@@ -0,0 +1,30 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+function CountryList({ cities, isLoading }) {
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (!cities.length) {
+    return (
+      <Message message="Add your first city by clicking on the city on the map" />
+    );
+  }
+  const countries = cities.reduce((acc, city) => {
+    if (!acc.map((el) => el.country).includes(city.country)) {
+      return [...acc, { country: city.country, emoji: city.emoji }];
+    } else {
+      return acc;
+    }
+  }, []);
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
