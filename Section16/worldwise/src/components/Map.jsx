876b944f1ce34644bfab6f2b715caa14b4ff261e
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-05-18 22:08:32 +0700

    Programming with Navigation with useNavigate and <Navigate/>

--- Section16/worldwise/src/components/Map.jsx
+++ Section16/worldwise/src/components/Map.jsx
@@ -1,13 +1,19 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   // Ham useSearchParams tra ve mot doi tuong SearchParams.
   // useSearchParams la mot hook cua react-router-dom, no giup chung ta truy cap vao cac tham so tren URL.
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
   return (
-    <div className={styles.mapContainer}>
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
       <h1>Map</h1>
       <p>
         Position: {lat} {lng}
